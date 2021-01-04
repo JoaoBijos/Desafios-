149f684c04422ffbcb50bbd9f5685906febdc07a
“JoaoBijos” <joaopedrobijos96@gmail.com>
2021-01-04 16:51:55 -0300

    Update

--- MoverDiv/javascript/script.js
+++ MoverDiv/javascript/script.js
@@ -6,6 +6,7 @@ function moveDiv(event){
     var tecla = event.keyCode; // acha o código da tecla 
     console.log(tecla) // usei pra verificar se estava achando as teclas corretamente 
 
+
     // 37 = esquerda
     // 38 = cima
     // 39 = direita
@@ -16,18 +17,22 @@ function moveDiv(event){
         eixoX -= 30;
         div1.style.left = eixoX + 'px';
         div1.style.backgroundColor = 'green';
+        document.body.style.backgroundColor = 'red'
     } else if(tecla === 38) {
         eixoY -= 30;
         div1.style.top = eixoY + 'px';
         div1.style.backgroundColor = 'blue';
+        document.body.style.backgroundColor = 'green'
     } else if(tecla === 39) {
         eixoX += 30;
         div1.style.left = eixoX + 'px';
         div1.style.backgroundColor = 'orange';
+        document.body.style.backgroundColor = 'blue'
     } else if(tecla === 40) {
         eixoY += 30;
         div1.style.top = eixoY + 'px';
         div1.style.backgroundColor = 'yellow';
+        document.body.style.backgroundColor = 'purple'
     }
 }
 
